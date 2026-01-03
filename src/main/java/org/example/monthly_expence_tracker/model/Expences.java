f29294ce0c52ca7107be7c30787dfe10558811f6
krishna887 <kpchaulagain1999@gmail.com>
2026-01-03 18:57:19 +0545

    added model

--- src/main/java/org/example/monthly_expence_tracker/model/Expences.java
+++ src/main/java/org/example/monthly_expence_tracker/model/Expences.java
@@ -1,4 +1,21 @@
 package org.example.monthly_expence_tracker.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
 public class Expences {
+  @Id
+  @GeneratedValue(strategy =  GenerationType.IDENTITY)
+  private int id;
+  private String title;
+  private Date date;
+  private double pricePerTitle;
+  private double totalPrice;
 }
